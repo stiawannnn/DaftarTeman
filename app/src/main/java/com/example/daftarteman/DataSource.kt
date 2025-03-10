a3bfaa0d4dd7fb4b39083f5bf55d2fab39def6f0
STIAWAN . 4 <darkmx123123@gmail.com>
2025-03-10 23:52:33 +0700

    done

--- app/src/main/java/com/example/daftarteman/DataSource.kt
+++ app/src/main/java/com/example/daftarteman/DataSource.kt
@@ -6,13 +6,22 @@ object DataSource {
         Friend(1, "Muhammad Ilham Safaruddin", "ilhamsafar@gmail.com", "Jl. Hangtuah Sp1 pir", R.drawable.ilham),
         Friend(2, "Fattah Fitriansyah", "fattah72722@gmail.com", "Jl.Hangtuah Sp1 Pir", R.drawable.fattah),
         Friend(3, "Ahmad Nugraha", "ahmadnugraha@gmail.com", "Jl. Bangkinang Kota", R.drawable.ahmad),
-        Friend(4, "Hariri Hasnul Habib", "haririhasnul@gmail.com", "Jl. Soekarno Hatta", R.drawable.habib),
-        Friend(5, "Jelita Aurelia", "jelitaaurelia@gmail.com", "Jl. Sudirman No. 12", R.drawable.jelita),
-        Friend(6, "Lukman Hakim", "lukmanhakim@gmail.com", "Jl. Merdeka Raya", R.drawable.lukman),
-        Friend(7, "Rani Dwi Sopia", "ranidwisopia@gmail.com", "Jl. Ahmad Yani", R.drawable.rani),
-        Friend(8, "Zhafira Ramadhani", "zhafiraramadhani@gmail.com", "Jl. Kartini No. 8", R.drawable.zhafira),
-        Friend(9, "Muhammad Zacky", "muhammadzacky@gmail.com", "Jl. Diponegoro", R.drawable.zaki),
-        Friend(10, "Rialin Ardiansyah", "rialinardiansyah@gmail.com", "Jl. Pattimura", R.drawable.rialin)
+        Friend(4, "Apri Supandi Pasaribu", "aprisupandi@gmail.com", "Jl. Sudirman Kota Pekanbaru", R.drawable.apri),
+        Friend(5, "Elfarina Fadwa Sucitra", "elfarinafadwa@gmail.com", "Jl. Garuda Sakti Panam", R.drawable.elfarina),
+        Friend(6, "Hariri Hasnul Habib", "haririhasnul@gmail.com", "Jl. Soekarno Hatta", R.drawable.habib),
+        Friend(7, "Haziel Wisma Attar", "hazielwisma@gmail.com", "Jl. HR. Soebrantas Panam", R.drawable.haziel),
+        Friend(8, "Jelita Aurelia", "jelitaaurelia@gmail.com", "Jl. Sudirman No. 12", R.drawable.jelita),
+        Friend(9, "Lukman Hakim", "lukmanhakim@gmail.com", "Jl. Merdeka Raya", R.drawable.lukman),
+        Friend(10, "M. Adli Ulhaq", "madliulhaq@gmail.com", "Jl. Tuanku Tambusai Panam", R.drawable.adli),
+        Friend(11, "M Wildan Alif", "mwildanalif@gmail.com", "Jl. Cipta Karya Panam", R.drawable.wildan),
+        Friend(12, "Muhammad Zacky", "muhammadzacky@gmail.com", "Jl. Diponegoro", R.drawable.zaki),
+        Friend(13, "Nazrul Ihsan", "nazrulihsan@gmail.com", "Jl. Kubang Raya Panam", R.drawable.nazrul),
+        Friend(14, "Putri Maharani", "putrimaharani@gmail.com", "Jl. Karya Bhakti Panam", R.drawable.putri),
+        Friend(15, "Rialin Ardiansyah", "rialinardiansyah@gmail.com", "Jl. Pattimura", R.drawable.rialin),
+        Friend(16, "Rikenro Aldifo", "rikenroaldifo@gmail.com", "Jl. Soebrantas Panam", R.drawable.riken),
+        Friend(17, "Rani Dwi Sopia", "ranidwisopia@gmail.com", "Jl. Ahmad Yani", R.drawable.rani),
+        Friend(18, "Zahra Annisa", "zahraannisa@gmail.com", "Jl. Uka Panam", R.drawable.zahra),
+        Friend(19, "Zhafira Ramadhani", "zhafiraramadhani@gmail.com", "Jl. Kartini No. 8", R.drawable.zhafira),
 
 
 
@@ -20,5 +29,8 @@ object DataSource {
 
 
 
-    )
+
+
+
+        )
 }
